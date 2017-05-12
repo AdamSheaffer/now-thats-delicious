3daad7889c7686b46aaca32ef8275bdc3243a962
Adam Sheaffer <JJ06896@JJ0000M301211.nash.tenn>
2017-05-12 15:48:28 -0500

    set up store controller

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
-router.get('/', (req, res) => {
-  res.render('hello');
-});
+router.get('/', storeController.homePage);
 
 module.exports = router;
