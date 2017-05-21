3b4dd793ec7867422cbd0d380224e66b145bf9b9
Adam Sheaffer <JJ06896@JJ0000M301211.local>
2017-05-21 14:42:30 -0500

    query stores for selected tag

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -87,11 +87,16 @@ exports.updateStore = async (req, res) => {
 };
 
 exports.getStoresByTag = async (req, res) => {
-    const tags = await Store.getTagsList();
+    const tagsPromise = Store.getTagsList();
     const tag = req.params.tag;
+    const tagQuery = tag || { $exists: true };
+    const storesPromise = Store.find({ tags: tagQuery });
+    const [tags, stores] = await Promise.all([tagsPromise, storesPromise]); 
+
     res.render('tags', {
         title: 'Tags',
+        tag,
         tags,
-        tag
+        stores
     });
 };
